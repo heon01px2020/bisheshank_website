dfaa39c8e94e160403c6ea49069e84b32b8e1c7a
bisheshank <bisheshank@brown.edu>
2024-07-04 18:08:22 -0400

    updated so that i remove all the autocomplete shit

--- my-app/src/TerminalComponent.js
+++ my-app/src/TerminalComponent.js
@@ -7,17 +7,12 @@ const TerminalComponent = () => {
   return (
     <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <Terminal
-        ref={terminalRef}
         commands={commands}
         welcomeMessage={'Welcome to my website! Type "help" to see all available commands.'}
         promptLabel={'me@bisheshank:~$'}
         autoFocus={true}
         errorText={'Command not found! Type "help" to see all available commands.'}
         styleEchoBack='fullInherit'
-        value={inputValue}
-        onChange={handleCommandChange}
-        onSubmit={handleCommandSubmit}
-        onKeyDown={handleKeyDown}
         style={{
           overflow: 'auto',
           width: '90%',
