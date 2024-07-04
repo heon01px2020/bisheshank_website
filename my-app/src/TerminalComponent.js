8a9b9ba4567adc413cec8f639a89d528140605be
heon01px2020 <heon01px2020@gmail.com>
2024-07-04 18:12:59 -0400

    a

--- my-app/src/TerminalComponent.js
+++ my-app/src/TerminalComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import Terminal from 'react-console-emulator';
 // import AutoComplete from './AutoComplete';
 import { commands } from './commands';
